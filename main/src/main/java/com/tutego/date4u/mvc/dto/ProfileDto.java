014183d6600db37e3ccf4118dc645526bda78e50
jmatd <jdmata@proton.me>
2022-10-14 13:39:49 +0200

    reworked profileDto

--- main/src/main/java/com/tutego/date4u/mvc/dto/ProfileDto.java
+++ main/src/main/java/com/tutego/date4u/mvc/dto/ProfileDto.java
@@ -2,5 +2,59 @@ package com.tutego.date4u.mvc.dto;
 
 import com.tutego.date4u.core.profile.Profile;
 
-public record ProfileDto(Profile profile,String profileImageName) {
+import java.time.LocalDate;
+import java.util.Objects;
+
+public final class ProfileDto {
+    private final Profile profile;
+    private final String profileImageName;
+    private final int ageInYears;
+
+
+    public ProfileDto(Profile profile, String profileImageName) {
+        this.profile = profile;
+        this.profileImageName = profileImageName;
+        this.ageInYears = calculateAgeInYears(profile.getBirthdate());
+    }
+
+    private int calculateAgeInYears(LocalDate birthdate) {
+        return birthdate.until(LocalDate.now()).getYears();
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public String getProfileImageName() {
+        return profileImageName;
+    }
+
+    public int getAgeInYears() {
+        return ageInYears;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ProfileDto) obj;
+        return Objects.equals(this.profile, that.profile) &&
+                Objects.equals(this.profileImageName, that.profileImageName) &&
+                this.ageInYears == that.ageInYears;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(profile, profileImageName, ageInYears);
+    }
+
+    @Override
+    public String toString() {
+        return "ProfileDto[" +
+                "profile=" + profile + ", " +
+                "profileImageName=" + profileImageName + ", " +
+                "ageInYears=" + ageInYears + ']';
+    }
+
+
 }
