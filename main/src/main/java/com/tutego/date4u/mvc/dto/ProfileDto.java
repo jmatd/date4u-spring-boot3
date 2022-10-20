555166671d358815087661c7ed1eb7ff3757e8e0
jmatd <jdmata@proton.me>
2022-10-20 11:06:19 +0200

    added pretty time

--- main/src/main/java/com/tutego/date4u/mvc/dto/ProfileDto.java
+++ main/src/main/java/com/tutego/date4u/mvc/dto/ProfileDto.java
@@ -1,20 +1,32 @@
 package com.tutego.date4u.mvc.dto;
 
 import com.tutego.date4u.core.profile.Profile;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import java.time.LocalDate;
+import java.util.Locale;
 import java.util.Objects;
 
 public final class ProfileDto {
+
+    private static final PrettyTime prettyTime = new PrettyTime(new Locale("de"));
+
     private final Profile profile;
     private final String profileImageName;
     private final int ageInYears;
+    private final String humanReadablyLastSeen;
+
 
 
     public ProfileDto(Profile profile, String profileImageName) {
         this.profile = profile;
         this.profileImageName = profileImageName;
         this.ageInYears = calculateAgeInYears(profile.getBirthdate());
+        this.humanReadablyLastSeen = prettyTime.format(profile.getLastseen());
+    }
+
+    public String getHumanReadablyLastSeen() {
+        return humanReadablyLastSeen;
     }
 
     private int calculateAgeInYears(LocalDate birthdate) {
