0fba8f23c78be33df54a7f1b812939253c3e761c
jmatd <jdmata@proton.me>
2022-10-26 13:41:23 +0200

    added login and logout eventlistener and index page

--- main/src/main/java/com/tutego/date4u/mvc/controllers/Date4uWebController.java
+++ main/src/main/java/com/tutego/date4u/mvc/controllers/Date4uWebController.java
@@ -1,12 +1,18 @@
 package com.tutego.date4u.mvc.controllers;
 
+import com.tutego.date4u.core.configuration.security.UnicornSecurityUser;
+import com.tutego.date4u.core.profile.Profile;
 import com.tutego.date4u.core.profile.ProfileRepository;
+import com.tutego.date4u.core.profile.ProfileService;
 import com.tutego.date4u.core.unicorn.Unicorn;
 import com.tutego.date4u.core.unicorn.UnicornRepository;
 import com.tutego.date4u.core.unicorn.UnicornService;
 import com.tutego.date4u.mvc.formdata.UnicornFormData;
 import jakarta.validation.Valid;
-
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,20 +30,32 @@ public class Date4uWebController {
     private final UnicornRepository unicornRepository;
     private final UnicornService unicornService;
 
-    public Date4uWebController(ProfileRepository profileRepository, UnicornRepository unicornRepository, UnicornService unicornService) {
+    private final ProfileService profileService;
+
+    public Date4uWebController(ProfileRepository profileRepository, UnicornRepository unicornRepository, UnicornService unicornService, ProfileService profileService) {
         this.profileRepository = profileRepository;
         this.unicornRepository = unicornRepository;
         this.unicornService = unicornService;
+        this.profileService = profileService;
     }
 
 
     @RequestMapping("/login")
-    public String loginPage() {
+    public String loginPage(@AuthenticationPrincipal UnicornSecurityUser unicornSecurityUser) {
+
         return "login";
     }
 
+    @RequestMapping("/logout")
+    public String logoutPage(@AuthenticationPrincipal UnicornSecurityUser unicornSecurityUser) {
+
+        return "redirect:/login?logout";
+    }
+
     @RequestMapping("/")
-    public String indexPage(Model model) {
+    public String indexPage(Model model, @AuthenticationPrincipal UnicornSecurityUser unicornSecurityUser) {
+        Profile profile = profileService.findById(unicornSecurityUser.getId());
+        model.addAttribute("profileName" ,profile.getNickname());
         long profileCount = profileRepository.count();
         model.addAttribute("totalProfiles", profileCount);
         return "index";
@@ -51,7 +69,7 @@ public class Date4uWebController {
     @PostMapping("/register")
     public String registerPagePost(Model model, @Valid UnicornFormData unicornFormData, BindingResult bindingResult) {
         if (unicornRepository.existsUnicornByEmail(unicornFormData.getEmail())) {
-            bindingResult.addError(new FieldError("unicornFormData", "email","E-mail bereits in verwendung"));
+            bindingResult.addError(new FieldError("unicornFormData", "email", "E-mail bereits in verwendung"));
         }
         if (bindingResult.hasErrors()) {
             return "register";
@@ -62,5 +80,6 @@ public class Date4uWebController {
         unicornService.createNewUnicorn(unicorn);
         return "redirect:/login?registered";
     }
+
 
 }
